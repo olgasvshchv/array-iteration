09b22485a28ebd3ae76dd50ac2e9e38e75476c33
olgasvshchv <olia.svishcheva@gmail.com>
2022-11-14 15:25:24 +0100

    navigation added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const body = document.querySelector('body');
+const nav = document.createElement('nav');
+const main = document.createElement('main');
 
+const navArray = [{text: 'Outlet', url:'https://midnatthome.com/product-category/outlet/'}, 
+{text: 'Journal', url:'https://midnatthome.com/journal/'},
+{text: 'Size Guide', url:'https://midnatthome.com/size-guide/'}];
 
+for (let i=0; i < navArray.length; i++) {
+  const button = document.createElement('a');
+  button.href = navArray[i].url;
+  button.innerHTML = navArray[i].text;
+  nav.appendChild(button);
+}
+body.insertAdjacentElement('afterbegin', nav);
+body.insertAdjacentElement('beforeend', main);
 const arrayImg = [{url:'https://midnatthome.com/wp-content/uploads/2021/11/paslakan2-midnatt_brownisch_1400px.jpg', text:'Hassel'},
 {url:'https://midnatthome.com/wp-content/uploads/2020/11/paslakan-wilted-midnatt_1170px.jpg',text:'Wilted'},
 {url:'https://midnatthome.com/wp-content/uploads/2020/11/Duvet-cover-set-kids-bosco-paslakan-barn-midnatt_LR-Grey-backgroung.jpg',text:'Olive'},
@@ -14,7 +27,7 @@ for (let i = 0; i < arrayImg.length; i++) {
   title.innerHTML = arrayImg[i].text;
   block.appendChild(img);
   block.appendChild(title);
-  body.appendChild(block);
+  main.appendChild(block);
   if (i%3===0) {
     title.style.color='white';
     block.style.backgroundColor='grey';
@@ -24,7 +37,7 @@ for (let i = 0; i < arrayImg.length; i++) {
       block.style.backgroundColor='white';
     }
 }
-body.style.display='flex';
-body.style.flexWrap='wrap';
+main.style.display='flex';
+main.style.flexWrap='wrap';
 
 
